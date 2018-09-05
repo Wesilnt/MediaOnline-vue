1ab09a96d239d6ade27e75e25612f670e1f45c6d
wulei <wulei@shbaoyuantech.com>
2018-09-05 11:35:49 +0800

    定义css reset

--- src/import.js
+++ src/import.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import { Swipe, SwipeItem, Header, Tabbar, TabItem, Button, InfiniteScroll ,Navbar} from 'mint-ui'
-import 'mint-ui/lib/style.css'
 
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
