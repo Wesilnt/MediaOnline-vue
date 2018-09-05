670bda536d03b92faaadf9e2835e75f65991d1a1
xiaoxubo <xiaoxubo@shbaoyuantech.com>
2018-09-05 11:19:09 +0800

    add my page

---
+++ src/views/my/purchase.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="purchase">
+        <mu-container>
+            <mu-expansion-panel>
+                <div slot="header">Panel 1</div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+                <mu-button slot="action" flat>Cancel</mu-button>
+                <mu-button slot="action" flat color="primary">Save</mu-button>
+            </mu-expansion-panel>
+            <mu-expansion-panel>
+                <div slot="header">Panel 2</div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </mu-expansion-panel>
+            <mu-expansion-panel>
+                <div slot="header">Panel 3</div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </mu-expansion-panel>
+        </mu-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "purchase"
+    }
+</script>
+
+<style scoped>
+
+</style>
