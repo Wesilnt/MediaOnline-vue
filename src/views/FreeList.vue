3d8c8f1e10012986e79b9835d1893a4ded76200d
MarkWangLike <wh197126@163.com>
2018-09-11 17:44:14 +0800

    免费列表重画播放按钮

--- src/views/FreeList.vue
+++ src/views/FreeList.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="free">
     <div v-for="(item, index) in freeList" :key="index" class="freeCell">
-      <div class="freeCellContent">
+      <div class="free-play-button-container">
         <div class="free-play-button">
-          <div class="bookPlayTringle"></div>
+          <div class="freePlayTringle">
+          </div>
+        </div>
         </div>
         <span class="content">{{item.type}}|{{item.title}}</span>
       </div>
-    </div>
     <div class="line" />
     <router-view name="FreeList" />
   </div>
@@ -21,27 +22,45 @@ export default {
 
 <style lang="less" scoped>
 .free {
-  margin-left: 28px;
   width: 694px;
   background-color: rgba(245, 245, 245, 0.5);
   border-radius: 14px;
+  height: 177px;
 }
 .free-cell {
   height: 88px;
   width: 100%;
-  padding-left: 20px;
+}
+.free-play-button-container{
+ display: inline-block;
+  width: 88px;
+  height: 88px;
+  vertical-align: middle;
+  margin-left: 28px;
 }
 .free-play-button {
   margin-top: 20px;
-  display: inline-block;
-  width: 68px;
-  height: 68px;
-  background-color: red;
+  width: 48px;
+  height: 48px;
+  background-color: #ffffff;
   border-radius: 50%;
+   -webkit-box-shadow: 0 0 15px 2px #e5dacf;
+  -moz-box-shadow: 0 0 15px 2px #e5dacf;
+  box-shadow: 0 0 15px 2px #e5dacf;
 }
-
+ .freePlayTringle {
+  // display: inline-block;
+  width: 0px;
+  height: 0px;
+  border-style: solid;
+  border-width: 10px 18px;
+  border-color: transparent;
+  border-left-color: #ffa32f;
+  position: relative;
+  top: 30%;
+  left: 35%;
+}
 .content {
-  line-height: 88px;
   font-size: 28px;
   color: rgb(22, 35, 60);
 }
@@ -50,7 +69,7 @@ export default {
   width: 654px;
   height: 2px;
   background-color: rgb(238, 238, 238);
-  margin-top: -99px;
+  margin-top: -88px;
   margin-left: 28px;
 }
 </style>
