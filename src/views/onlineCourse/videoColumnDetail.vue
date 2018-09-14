846025323e3e5e108bfaf1ddc4d92d2cd6ab1260
qhht <491937754@qq.com>
2018-09-14 18:23:45 +0800

    修改专栏详情页面布局

--- src/views/onlineCourse/videoColumnDetail.vue
+++ src/views/onlineCourse/videoColumnDetail.vue
@@ -1,65 +1,66 @@
 <template>
-    <div class="videocol-dec-container" id="detailmain" ref="detailmain">
-       <div class="videocol-header" :style="{ background : 'url('+profilePic+')' }">
-            <span class="videocol-header-bottom">{{buyCount}}人已购买</span>
-       </div>
-
-        <div class="videocol-navbar" id="navbar" ref="navbar">
-            <div v-for="(item,index) of navbar" :class="{'selected':selected == index }" :key="index" class="videocol-navbar-item" @click="clickFnc(index)">{{item}}</div>
-        </div>
-            <tools-navbar
-                v-on:router-to-audition="routerToAudition"
-                v-on:router-to-collage="routerToCollage"
-                v-on:router-to-collect="routerToCollect"
-                :price="['100','10']"
-                collageText="拼团拼团"
-                :collect='true'
-                :collage='true'
-            />
-
-
-       <div class="videocol-content">
-           <course-introduce ref="desc" id="desc" :courseinfo="description"/>
-           <hr class="lineone">
-            <div class="videocol-sction-title">
-                <h4>课程列表 <label>(共{{lessonCount}}讲)</label></h4>
-           </div>
-            <div class="videocol-bigimage">
-                <vue-bigimage :src="outlinePic"/>   
-                <img :src="require('../../assets/images/onlinecourse_bigimage_search.png')" class="videocol-bigimage-search">
-            </div>
-            <hr class="lineone">
-            <div class="videocol-sction-title" id="tryCourse" ref="tryCourse">
-                <h4>试看课程</h4>
-                <div class="videocol-all" @click="allFunc">
-                    <span class="videocol-allbtn">全部</span>
-                    <img :src="require('../../assets/images/onlinecourse_arrow_right.png')" class="videocol-allbtn-icon">
-                </div>
-           </div>
-
-            <playlist v-for="(item,index) of freeLessonList" :key="item.id" :iteminfo="item" :lastindex="index === (freeLessonList.length - 1)" @jumpEvent="gotoVideoCourseDetailPage(item.id)"/>
-
-            <hr class="lineone">
-            <div class="videocol-sction-title" id="leavemessage" ref="leavemessage">
-                <h4>精选留言</h4>
-                <div class="videocol-all" @click="allFunc">
-                    <span class="videocol-allbtn">{{commentCount}}条</span>
-                    <img :src="require('../../assets/images/onlinecourse_arrow_right.png')" class="videocol-allbtn-icon">
-                </div>
-           </div>
-           <!-- <video-comment v-for="item of videoColumnComments" :key="item.id" :comment="item"/> -->
-            <commentitem class="video-column-comment" v-for="(item,index) of videoColumnComments" :key="index" :comment="item" :unindent="true" :regiontype="2201"/>
-            <hr class="lineone">
-            <div class="videocol-sction-title">
-                <h4>购买须知</h4>
-           </div>
-           <div class="videocol-purchase-tip-fatherView">
-               <p v-html="buyIntro"></p>
-           </div>
-
-            
-       </div>
+  <div class="videocol-dec-container" id="detailmain" ref="detailmain">
+    <!-- Header -->
+    <div class="videocol-header" :style="{ background : 'url('+profilePic+')' }">
+      <span class="videocol-header-bottom">{{buyCount}}人已购买</span>
     </div>
+    <!-- Navbar -->
+    <div class="videocol-navbar" id="navbar" ref="navbar">
+        <div v-for="(item,index) of navbar" :class="{'selected':selected == index }" :key="index" class="videocol-navbar-item" @click="clickFnc(index)">{{item}}</div>
+    </div>
+    <!-- 底部工具条 -->
+    <tools-navbar
+        v-on:router-to-audition="routerToAudition"
+        v-on:router-to-collage="routerToCollage"
+        v-on:router-to-collect="routerToCollect"
+        :price="['100','10']"
+        collageText="拼团拼团"
+        :collect='true'
+        :collage='true'
+    />
+    <!-- description(介绍) -->
+    <div class="videocol-base">
+       <course-introduce ref="desc" id="desc" :courseinfo="description"/>
+    </div>
+    <!-- 课程列表 -->
+    <div class="videocol-bigimage">
+      <div class="videocol-sction-title">
+        <h4>课程列表 <label>(共{{lessonCount}}讲)</label></h4>
+      </div>
+      <vue-bigimage :src="outlinePic"/>   
+      <img :src="require('../../assets/images/onlinecourse_bigimage_search.png')" class="videocol-bigimage-search">
+    </div>
+    <!-- 试看课程 -->
+    <div class="videocol-base">
+      <div class="videocol-sction-title" id="tryCourse" ref="tryCourse">
+        <h4>试看课程</h4>
+        <div class="videocol-all" @click="allFunc">
+          <span class="videocol-allbtn">全部</span>
+          <img :src="require('../../assets/images/onlinecourse_arrow_right.png')" class="videocol-allbtn-icon">
+        </div>
+      </div>
+      <playlist v-for="(item,index) of freeLessonList" :key="item.id" :iteminfo="item" :lastindex="index === (freeLessonList.length - 1)" @jumpEvent="gotoVideoCourseDetailPage(item.id)"/>
+    </div>
+    <!-- 精选留言 -->
+    <div class="videocol-base">
+      <div class="videocol-sction-title" id="leavemessage" ref="leavemessage">
+        <h4>精选留言</h4>
+        <div class="videocol-all" @click="allFunc">
+            <span class="videocol-allbtn">{{commentCount}}条</span>
+            <img :src="require('../../assets/images/onlinecourse_arrow_right.png')" class="videocol-allbtn-icon">
+        </div>
+      </div>
+      <commentitem class="video-column-comment" v-for="(item,index) of videoColumnComments" :key="index" :comment="item" :regiontype="2201"/>
+    </div>
+    <div class="videocol-base">
+      <div class="videocol-sction-title">
+        <h4>购买须知</h4>
+      </div>
+      <div class="videocol-purchase-tip-fatherView">
+        <p v-html="buyIntro"></p>
+      </div>  
+    </div>
+  </div>
 </template>
 
 <script>
@@ -115,19 +116,11 @@ export default {
     },
     async handleScroll() {
       //1.监听滚动
-      // var scrollTop =
-      //     window.pageYOffset ||
-      //     document.documentElement.scrollTop ||
-      //     document.body.scrollTop
       let scrollTop = Math.abs(
         this.$refs.detailmain.getBoundingClientRect().top
       )
       let tryCourseH = this.$el.querySelector('#tryCourse').offsetTop - 50
       let messageH = this.$el.querySelector('#leavemessage').offsetTop - 50
-      console.log('==============')
-      console.log(scrollTop)
-      console.log(tryCourseH)
-      console.log(messageH)
       if (scrollTop < tryCourseH) {
         this.selected = 0
       } else if (scrollTop < messageH && scrollTop > tryCourseH) {
@@ -199,6 +192,7 @@ export default {
   background-color: #fff;
   height: 100%;
 }
+// Header
 .videocol-header {
   width: 100%;
   height: 300px;
@@ -248,53 +242,21 @@ export default {
     border-bottom: 6px solid rgb(255, 163, 47);
   }
 }
-
-///////////////////////////
-.videocol-content {
+//介绍
+.videocol-base {
   padding: 0 40px;
   background-color: #fff;
-  a {
-    text-decoration: none;
-  }
-}
-
-.videocol-sction-title {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  font-size: 32px;
-  text-align: left;
-  color: rgb(62, 62, 83);
-  margin-top: 48px;
-  align-items: center;
-}
-
-//分割线
-.lineone {
-  margin-left: -40px;
-  margin-right: -40px;
-  width: auto;
-  height: 8px;
-  background-color: rgb(247, 247, 247);
-  border: none;
-}
-
-.videocol-sction-title h4 {
-  margin: 0;
-  font-size: 32px;
-  color: rgb(62, 62, 83);
-}
-.videocol-sction-title h4 label {
-  font-size: 28px;
-  color: rgb(155, 161, 176);
+  border-bottom: 8px solid rgb(247, 247, 247);
 }
 //大图浏览
 .videocol-bigimage {
   position: relative;
+  padding: 0 40px;
   width: 100%;
   height: 800px;
-  margin-bottom: 40px;
+  margin: 40px 0;
   background-color: #fff;
+  border-bottom: 8px solid rgb(247, 247, 247);
 }
 
 .videocol-bigimage-bg {
@@ -309,15 +271,32 @@ export default {
   position: absolute;
   width: 80px;
   height: 80px;
-  right: 20px;
-  bottom: 20px;
+  right: 40px;
+  bottom: 40px;
 }
 
-//留言评论部分
-.video-column-comment {
-  margin-top: 56px;
+//区头
+.videocol-sction-title {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  font-size: 32px;
+  text-align: left;
+  color: rgb(62, 62, 83);
+  margin-top: 48px;
+  align-items: center;
 }
 
+.videocol-sction-title h4 {
+  margin: 0;
+  font-size: 32px;
+  color: rgb(62, 62, 83);
+}
+.videocol-sction-title h4 label {
+  font-size: 28px;
+  color: rgb(155, 161, 176);
+}
+//试看课程
 .videocol-all {
   display: flex;
   flex-direction: row;
@@ -336,6 +315,13 @@ export default {
   width: 16px;
   height: 28px;
 }
+
+//精选留言
+.video-column-comment {
+  margin: 56px 0 40px;
+}
+
+//购买须知
 .videocol-purchase-tip {
   font-size: 32px;
   color: rgb(128, 128, 128);
@@ -345,6 +331,7 @@ export default {
   width: 100%;
 }
 .videocol-purchase-tip-fatherView :last-child {
-  margin-bottom: 260px;
+  margin-bottom: 60px;
 }
+
 </style>
