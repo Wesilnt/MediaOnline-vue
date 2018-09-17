666e83a50b69bef2c35240d88fb367d74b4ac159
qhht <491937754@qq.com>
2018-09-17 18:07:51 +0800

    添加拼团UI

---
+++ src/components/CountDown.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="qhht-flex countDown-container">
+        <span class="time-number">23</span>
+        <span class="time-symbol">:</span>
+        <span class="time-number">27</span>
+        <span class="time-symbol">:</span>
+        <span class="time-number">05</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'CountDown'
+}
+</script>
+
+
+<style lang="scss" scoped>
+.countDown-container {
+    flex-direction: row;
+    justify-content: center;
+}
+.time-number {
+    width:64px;
+    height: 48px;
+    background-color: rgb(250, 219, 162);
+    border-radius: 8px;
+    font-size: 36px;
+    color: white;
+    text-align: center;
+}
+.time-symbol {
+    background-color:white;
+    font-size: 36px;
+    color: black;
+    text-align: left;
+    margin:0 9px;
+}
+</style>
+
