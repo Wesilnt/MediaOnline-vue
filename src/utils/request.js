9ff7f5f1937ff9c58a52827757124f8242635b70
MarkWangLike <wh197126@163.com>
2018-09-18 19:55:54 +0800

    调试

--- src/utils/request.js
+++ src/utils/request.js
@@ -73,11 +73,11 @@ const checkResponseCode = (url, response) => {
 }
 function GetRequestCode() {
   var url = location.search; //获取url中"?"符后的字符串
-  console.log(url)
   var theRequest = new Object();
   if (url.indexOf("?") != -1) {
     var str = url.substr(1);
     strs = str.split("&");
+    console.log(strs)
     for (var i = 0; i < strs.length; i++) {
       theRequest[strs[i].split("=")[0]] = (strs[i].split("=")[1]);
     }
