e2306d5696f68bf56b93fb042d3258cd373f39d6
MarkWangLike <wh197126@163.com>
2018-09-20 10:27:46 +0800

    调试

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import {getToken} from './utils/request.js'
 export default {
   name: 'App',
   beforeCreate(){ console.log('程序加载前')
-  //  getToken()
+   getToken()
   },
   created(){
     console.log('程序加载')
