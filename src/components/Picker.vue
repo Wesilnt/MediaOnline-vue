ab0398e6c0cefdc422fbc37656c4856ab2fff054
xiaoxubo <xiaoxubo@shbaoyuantech.com>
2018-09-20 15:36:40 +0800

    add DisplayUserInfo

---
+++ src/components/Picker.vue
@@ -0,0 +1,48 @@
+<template>
+    <van-popup v-model="isShow" position="bottom" >
+        <van-picker
+                show-toolbar
+                :columns="columns"
+                @confirm="onConfirm"
+                @cancel="isShow=false"
+        />
+    </van-popup>
+</template>
+
+<script>
+  import default_left_icon from '../assets/audio_cmt_speak.png'
+  import default_right_icon from '../assets/audio_cmt_text.png'
+
+  export default {
+    name: 'Picker',
+    props: {
+      columns: {
+        default:[]
+      },
+      isShow: {
+        default: false
+      }
+    },
+    data: function() {
+      return {
+      }
+    },
+    methods: {
+      onConfirm(value, index) {
+        this.isShow = false
+        this.$emit('toggle', value)
+      },
+    }
+  }
+</script>
+
+<style lang="less">
+    .van-picker-column__item {
+        width: 100vw
+
+    }
+    .van-picker-column{
+        overflow: visible;
+    }
+
+</style>
--- src/views/my/DisplayUserInfo.vue
+++ src/views/my/DisplayUserInfo.vue
@@ -1,33 +1,42 @@
 <template>
    <div class="display-user-info">
-       <div class="display-user-info-column">
+       <div class="display-user-info-column" @click="handleUpdateUserInfo('gender')">
            <p class="display-user-info-column-label">性别</p>
-           <p class="display-user-info-column-content">}男</p>
+           <p class="display-user-info-column-content">男</p>
            <img class="display-user-info-column-image-arraw" src="../../assets/images/onlinecourse_arrow_right.png"/>
        </div>
-       <div class="display-user-info-column">
+       <div class="display-user-info-column" @click="handleUpdateUserInfo('grade')">
            <p class="display-user-info-column-label">年级</p>
            <p class="display-user-info-column-content">三年级</p>
            <img class="display-user-info-column-image-arraw" src="../../assets/images/onlinecourse_arrow_right.png"/>
        </div>
-       <div class="display-user-info-column">
+       <div class="display-user-info-column" @click="handleUpdateUserInfo('phone')">
            <p class="display-user-info-column-label">手机号码</p>
            <p class="display-user-info-column-content">151****5465</p>
            <img class="display-user-info-column-image-lock" src="../../assets/images/my_userinfo_lock.png"/>
        </div>
+       <Picker :columns="whichPicker ==='gender' ? genders : grades" :isShow="isShow" v-on:toggle="toggleConfirm"/>
    </div>
 </template>
 
 <script>
   import { createNamespacedHelpers } from 'vuex'
-
-  const { mapState, mapActions, mapGetters } = createNamespacedHelpers(
+  import Picker from '../../components/Picker'
+  import { Toast } from 'vant'
+  const { mapState, mapActions } = createNamespacedHelpers(
     'userInfo'
   )
   export default {
     name: "DisplayUserInfo",
+    components: {
+      Picker
+    },
     data: function() {
       return {
+        whichPicker:'',
+        isShow:false,
+        genders:['男','女'],
+        grades: ['未上学', '幼儿园', '一年级', '二年级', '三年级','四年级','五年级','六年级','初一','初二','初三','初三以上']
       }
     },
     computed: {
@@ -35,10 +44,31 @@
     },
     methods: {
       ...mapActions(['getMyUserInfo', 'updateUserInfo']),
-      handleUpdateUserInfo:function () {
-        console.log("handleUpdateUserInfo")
-      }
+      handleUpdateUserInfo:function (type) {
+        if (type === 'gender') {
+            this.whichPicker = 'gender'
+            this.isShow = true
+        } else if ( type === 'grade') {
+            this.whichPicker = 'grade'
+            this.isShow = true
+        } else if( type === 'phone') {
+          Toast.fail('电话号码无法编辑')
+        }
+      },
+      toggleConfirm:function(value) {
+        console.log(value);
+        if(this.genders.indexOf(value) != -1 ) {
+          if (!( value === this.userInfo.gender)) {
+            this.updateUserInfo(value)
+          }
+        } else if(this.grades.indexOf(value) != -1){
+          if (!( value === this.userInfo.grade)) {
+            this.updateUserInfo(value)
+          }
+        }
+      },
     },
+
     created() {
       this.getMyUserInfo()
     }
@@ -55,19 +85,19 @@
             padding: 46px 0px 46px 0px;
             border-bottom: 0.1px solid #ddd;
             &-label {
-
+                color:#3e3e53;
             }
             &-content {
-                font-size: 27px;
                 position: absolute;
                 right: 90px;
+                color: #959899;
             }
             &-image-arraw {
                 width: 15px;
                 height: 28px;
                 position: absolute;
                 right: 30px;
-                margin: 0px 16px 0px 0px;
+                margin: 5px 16px 0px 0px;
             }
             &-image-lock {
                 width: 26px;
