a9b5d52435b135d5ea1c7fef10b47043b898786e
xiaoxubo <xiaoxubo@shbaoyuantech.com>
2018-09-21 11:08:51 +0800

    change api url

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ let api = 'https://xcx.test.shbaoyuantech.com:30000'
 if (NODE_ENV === 'development') {
   api = 'https://xcx.test.shbaoyuantech.com:30000'
 }else if (NODE_ENV === 'production') {
-  // api = 'https://xcx.shbaoyuantech.com'
+  api = 'https://xcx.shbaoyuantech.com'
 }else{
   throw new Error('域名地址既不是测试环境也不是正式环境')
 }
