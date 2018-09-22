95d647a12a986dce1d7e6d9dca3f4fe0c102da90
MarkWangLike <wh197126@163.com>
2018-09-22 18:59:07 +0800

    提交配置

--- src/components/share/Share.vue
+++ src/components/share/Share.vue
@@ -46,7 +46,26 @@ export default {
       this.isOpen = value
     }
   },
+      mounted() {
+        this.config();
+       
+    },
   methods: {
+    async config(){
+  let result0= await wxConfig({'url' : window.location.href})
+ console.log('config' +result0);
+        let config = result0.js_config
+        console.log('嫩不能走到这里')
+        console.log(result0)
+        wx.config({
+          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: config.appid, // 必填，企业号的唯一标识，此处填写企业号corpid
+          timestamp: config.timestamp, // 必填，生成签名的时间戳
+          nonceStr: config.nonceStr, // 必填，生成签名的随机串
+          signature: config.signature, // 必填，签名，见附录1
+          jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        })
+    },
     onShareItem(shareScore) {
       this.isOpen = false
       if (shareScore == 'poster') {
@@ -82,20 +101,7 @@ export default {
         this.$toast('分享到朋友圈')
       }
     },
-    mounted() {
-          let result0 = wxConfig({ url: window.location.href })
-        let config = result0.js_config
-        console.log('嫩不能走到这里')
-        console.log(result0)
-        wx.config({
-          debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-          appId: config.appid, // 必填，企业号的唯一标识，此处填写企业号corpid
-          timestamp: config.timestamp, // 必填，生成签名的时间戳
-          nonceStr: config.nonceStr, // 必填，生成签名的随机串
-          signature: config.signature, // 必填，签名，见附录1
-          jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-        })
-    },
+
     onCancel() {
       this.isOpen = false
     },
