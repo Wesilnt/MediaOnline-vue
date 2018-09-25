8c49456eefcea61bb02d219ee746dd7ff88ea7d5
qhht <491937754@qq.com>
2018-09-25 17:59:40 +0800

    调试支付

--- src/components/toolsNavbar.vue
+++ src/components/toolsNavbar.vue
@@ -129,7 +129,7 @@ export default {
           'payType' : 2
         }
       }
-      console.log('userAccessStatus =',userAccessStatus)
+      console.log('userAccessStatus =',this.userAccessStatus)
       switch(this.userAccessStatus) {
         case -3:
           //拼团失败,重新发起拼团
