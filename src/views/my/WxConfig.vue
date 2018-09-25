c95a43309c89732316fb5ffad7d4404386d6ce9e
wulei <wulei@shbaoyuantech.com>
2018-09-25 15:58:40 +0800

    微信分享 测试

--- src/views/my/WxConfig.vue
+++ src/views/my/WxConfig.vue
@@ -1,29 +1,42 @@
 <template>
     <div class="btn-wrapper">
         <van-button type="default" size="large" @click="wxConfig">config</van-button>
-        <van-button type="primary" size="large">主要按钮</van-button>
+        <van-button type="primary" size="large"  @click="wxShare">分享按钮</van-button>
         <van-button type="warning" size="large">警告按钮</van-button>
         <van-button type="danger" size="large">危险按钮</van-button>
     </div>
 </template>
 
 <script>
-import { wxConfig } from '../../utils/wxConfig'
+import { wxConfig, wxShare } from '../../utils/wxConfig'
 export default {
   name: 'wxConfig',
-    methods:{
-        wxConfig:()=>{
-            wxConfig()
+  methods: {
+    wxConfig: () => {
+      wxConfig()
+    },
+    wxShare: () => {
+      wxShare(
+        {
+          title: '这是配置页的分享',
+          desc: 'Vue 我又更新npm了',
+          link: window.href,
+          imgUrl: require('../../assets/share_poster.png')
+        },
+        res => {
+          console.log(res)
         }
+      )
     }
+  }
 }
 </script>
 
 <style scoped>
-    .btn-wrapper{
-        margin: 24px;
-    }
-    .van-button{
-        margin-bottom: 12px;
-    }
+.btn-wrapper {
+  margin: 24px;
+}
+.van-button {
+  margin-bottom: 12px;
+}
 </style>
