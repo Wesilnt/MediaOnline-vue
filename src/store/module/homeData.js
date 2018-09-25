3e6779e091a3500f8799db7b4adc7f1bd66d8ba2
wulei <wulei@shbaoyuantech.com>
2018-09-25 11:50:34 +0800

    token

--- src/store/module/homeData.js
+++ src/store/module/homeData.js
@@ -26,32 +26,26 @@ const homeData = {
       await dispatch('getBookList')
     },
     async getBannerList({ commit }) {
-      console.log('首页接口1')
       let result = await getBannerList({ type: 0 })
       commit('setBannerList', result)
     },
     async getFreeList({ commit }) {
-      console.log('首页接口2')
       let result = await getFreeList({ pageSize: 2 })
       commit('setFreeList', result.result)
     },
     async getNewMessageCount({ commit }) {
-      console.log('首页接口3')
       let result = await getNewMessageCount({ busiTypes: 3101 })
       commit('setNewMessageCount', result.data)
     },
     async getVisionList({ commit }) {
-      console.log('首页接口4')
       let result = await getVisionList()
       commit('setVisionList', result)
     },
     async getVideoList({ commit }) {
-      console.log('首页接口6')
       let result = await getVideoList()
       commit('setVideoList', result)
     },
     async getBookList({ commit }) {
-      console.log('首页接口7')
       let result = await getBookList()
       commit('setBookList', result)
     }
