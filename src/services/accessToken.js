70ecebf97c408b3b340a8bf0506af0d6c1f137bd
wulei <wulei@shbaoyuantech.com>
2018-09-27 15:02:03 +0800

    ENV online

--- src/services/accessToken.js
+++ src/services/accessToken.js
@@ -3,8 +3,9 @@ import { ISONLINE } from '../utils/config'
 
 export async function getToken() {
   console.log(window.location.href)
+    const originHref=window.location.href.split('#')[1]
   const result = await request.post('/auth/wechat/get-wechat-auth-link', {
-    originUrl: window.location.href
+    originUrl:'http://t.shbaoyuantech.com' +originHref
   })
   console.log(result)
   if (ISONLINE) window.location.href = result.wechatAuthUrl
