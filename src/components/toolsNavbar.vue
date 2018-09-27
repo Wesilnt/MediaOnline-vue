9ee3a5d962ec464dc90c116c49b1a62eadca9a85
qhht <491937754@qq.com>
2018-09-27 14:14:26 +0800

    调试拼团

--- src/components/toolsNavbar.vue
+++ src/components/toolsNavbar.vue
@@ -172,8 +172,13 @@ export default {
           break
         case 1003:
           //拼团成功.解锁专栏,跳转到单集详情页
-          const lessonId = this.freeLessonList[0].id
-          this.$router.push({ name: 'videoCourseDetail', params: { lessonId } })
+          if(this.freeLessonList[0]){
+            const lessonId = this.freeLessonList[0].id
+            this.$router.push({ name: 'videoCourseDetail', params: { lessonId } })
+          }else
+          {
+            console.log('没有试听课程')
+          }
           break
         case 1005:
           //拼团中
