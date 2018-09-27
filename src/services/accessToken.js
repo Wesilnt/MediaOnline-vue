7479d566ead74e5a6bf94b90c212919cb369653b
wulei <wulei@shbaoyuantech.com>
2018-09-27 10:52:25 +0800

    console accessToken.js

--- src/services/accessToken.js
+++ src/services/accessToken.js
@@ -1,9 +1,11 @@
 import request from '../utils/request'
 
 export async function getToken() {
-  const result = await request.post('/auth/wechat/get-wechat-auth-link', {
+    console.log(window.location.href);
+    const result = await request.post('/auth/wechat/get-wechat-auth-link', {
     originUrl: window.location.href
   })
-  window.location.href = result.wechatAuthUrl
+    console.log(result);
+    window.location.href = result.wechatAuthUrl
   return result
 }
