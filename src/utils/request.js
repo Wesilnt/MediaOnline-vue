b850e00a5305b4229f1f7a4ec22bb3920e1e0e25
qhht <491937754@qq.com>
2018-09-28 14:26:31 +0800

    调试集赞

--- src/utils/request.js
+++ src/utils/request.js
@@ -98,7 +98,7 @@ function request(url, options, needToken = true) {
     accessToken =
         ISONLINE
         ? getCookie('COOKIE_TOKEN_KEY_CNONLINE')
-        : "e4756a02-9571-431d-ab0f-c315479fccad"
+        : "f9ec36d8-1e0d-41ff-8bb0-5209fbcfa9a4"
     defaultOptions = {
       // credentials: 'include',
       // mode: 'no-cors',
