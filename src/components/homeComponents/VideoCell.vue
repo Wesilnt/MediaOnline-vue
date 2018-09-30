f13a9cf5cafe4ad77a8a8d01315f4ff25ef77151
xiaoxubo <xiaoxubo@shbaoyuantech.com>
2018-09-30 15:48:44 +0800

    add code

--- src/components/homeComponents/VideoCell.vue
+++ src/components/homeComponents/VideoCell.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-lazy:background-image="video.coverPic" @click="onItemClick" class="qhht-flex lazy-img-larger cell">
+  <div v-lazy:background-image="video.coverPicH" @click="onItemClick" class="qhht-flex lazy-img-larger cell">
       <i class="qhht-icon videoPlayTringle" ></i>
     <p class="videoTitle">{{video.name}}</p>
     <p>{{video.briefIntro}}</p>
--- src/views/my/EditUserPhone.vue
+++ src/views/my/EditUserPhone.vue
@@ -1,12 +1,16 @@
 <template>
     <section>
-        <p class="userinfo-title">欢迎使用秦汉胡同 </p>
+        <p class="userinfo-title">{{isInputPhone ? '欢迎使用秦汉胡同' : '验证码已发送'}} </p>
         <div class="userinfo-phone">
-            <div v-if="isInputPhone" class="userinfo-phone-container">
-                <input class="userinfo-phone-container-input-phone" v-model="mobileNumber" type="number" name="points" maxlength="11" placeholder="请输入手机号"/>
+            <div v-if="isInputPhone" class="userinfo-phone-input-container">
+                <input class="userinfo-phone-input-container-phone" v-model="mobileNumber" type="number" name="points" maxlength="11" placeholder="请输入手机号"/>
             </div>
-            <div v-if="!isInputPhone" class="userinfo-phone-container">
-              <p class="userinfo-phone-container-phone-text">{{ mobileNumber }}</p>
+            <div v-if="!isInputPhone" class="userinfo-phone-code-container">
+                <div class="userinfo-phone-code-container-content">
+                    <p class="userinfo-phone-code-container-content-text">{{ mobileNumber }}</p>
+                    <p class="userinfo-phone-code-container-content-countdown">({{ countDownNumber }}秒)</p>
+                </div>
+
                 <div class="v-code">
                     <input
                             ref="vcode"
@@ -49,11 +53,12 @@
       return {
         isInputPhone:true,
         phone: '',
+        countDownNumber: 60,
         validCode: '',
         mobileNumber: '',
         validateCode: '',
         code: '',
-        codeLength: 4,
+        codeLength: 6,
         telDisabled: false,
         focused: false
       }
@@ -70,7 +75,7 @@
     watch: {
       code(newVal) {
         this.code = newVal.replace(/[^\d]/g,'')
-        if (newVal.length > 3) {
+        if (newVal.length > 5) {
           // this.telDisabled = true
           this.$refs.vcode.blur()
           setTimeout(() => {
@@ -92,6 +97,20 @@
       handleNext: function () {
         this.sendCode()
         this.isInputPhone = false
+        this.countDown()
+      },
+      countDown:function () {
+        let num = 60
+        let that = this
+        clearInterval(timer)
+        var timer = setInterval(function() {
+          num--
+          that.countDownNumber = num
+          if (num === 0) {
+            //that.countDownNumber
+            clearInterval(timer)
+          }
+        }, 1000)
       }
     }
   }
@@ -111,9 +130,9 @@
                 font-size: 28px;
                 color: #a3a8b6;
             }
-            &-container {
+            &-input-container {
                 padding: 40px 60px 50px 60px;
-                &-input-phone {
+                &-phone {
                     width: 100%;
                     height: 80px;
                     text-align:center;
@@ -126,10 +145,34 @@
                     border-bottom-width: 1px;
                     font-size: 36px;
                 }
+                &-text{
+                    font-size: 36px;
+                    padding: 0px 0px 80px 0px;
+                    letter-spacing: 3px;
+                }
                 &-phone-text{
                     font-size: 36px;
                     padding: 0px 0px 80px 0px;
                     letter-spacing: 3px;
+                }
+            }
+            &-code-container {
+                padding: 40px 60px 50px 60px;
+                &-content{
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-between;
+                    padding-bottom: 80px;
+                    &-text {
+                        height: 80px;
+                        letter-spacing: 3px;
+                        font-size: 36px;
+                    }
+                    &-countdown{
+                        font-size: 36px;
+                        padding-right: 80px;
+                        color: #ced4da;
+                    }
                 }
             }
         }
@@ -178,11 +221,11 @@
     }
     .v-code .line {
         position: relative;
-        width: 60px;
+        width: 64px;
         height: 80px;
         line-height: 32px;
         text-align: center;
-        font-size: 96px;
+        font-size: 80px;
     }
     .v-code .line::after {
         display: block;
