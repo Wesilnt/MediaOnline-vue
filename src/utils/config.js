ec618c836301eaedab4fcb052d80b8afb77fda3a
qhht <491937754@qq.com>
2018-09-30 14:54:55 +0800

    测试

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 const { NODE_ENV } = process.env
 
 // 调试选项，本地调试请改为 False
-export const IS_ONLINE = false
+export const IS_ONLINE = true
 
 // TODO router 设置未统一，后期需要统一格式
 export const courseType = {
