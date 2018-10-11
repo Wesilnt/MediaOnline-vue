a9fa047106ab5f1d81960b7981d9b8c7e9992587
MarkWangLike <wh197126@163.com>
2018-10-11 10:08:21 +0800

    试看列表使用小缩略图

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 const { NODE_ENV } = process.env
 
 // 调试选项，本地调试请改为 False
-export const IS_ONLINE = true
+export const IS_ONLINE = false
 // 测试 Token
 export const TEST_TOKEN = "0b538534-0cb5-4ddf-8ec1-e7cc1a444f93"
 
