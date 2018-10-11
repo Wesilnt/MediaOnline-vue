bf24db9d4c376c1d9811645458879fc4b3948ba5
liuchao <the_liuchao@sian.cn>
2018-10-11 16:43:09 +0800

    分享设置2

--- src/views/onlineCourse/videoColumnDetail.vue
+++ src/views/onlineCourse/videoColumnDetail.vue
@@ -60,7 +60,7 @@ export default {
       //拼装分享内容
       this.shareData = {
         link: this.url + `/#/videoColumnDetail/${this.courseId}`, 
-        title: `我是${user.nickName}, 邀请你一起收听《${this.courseName}》`,
+        title: `${this.courseName}`,
         desc: '你一定会爱上国学课...',
         successCB: () => console.log('分享回调成功') ,
         cancelCB: () =>  console.log('分享回调失败')
