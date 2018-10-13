4d581b4bda8e6c8523cfac4405b941696901cae3
qhht <491937754@qq.com>
2018-10-13 17:41:59 +0800

    调试分享

--- src/store/module/groupManagerData.js
+++ src/store/module/groupManagerData.js
@@ -149,6 +149,9 @@ const groupManagerData = {
     actions:{
 
         setupShareOption({state,dispatch,rootState},{courseName,courseId,groupBuyId,collectLikeId,leavePerson,orderStatus}){
+            console.log('isFromShare =',isFromShare)
+            console.log('orderStatus =',orderStatus)
+            console.log('userAccessStatus =',state.userAccessStatus)
             if(state.isFromShare){
                 dispatch('getUserInfo',null,{root:true}).then(user =>{
                     let title = null
