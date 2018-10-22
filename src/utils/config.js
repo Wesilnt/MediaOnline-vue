26e86e4e5ce1209b6ef6049f28a8ad83d00940d9
wulei <wulei@shbaoyuantech.com>
2018-10-22 14:31:49 +0800

    debug

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 const { NODE_ENV } = process.env
 
 // 调试选项，本地调试请改为 False
-export const IS_ONLINE = false
+export const IS_ONLINE = true
 // 测试 Token
 export const TEST_TOKEN = "b441dc5a-c00f-49b3-89fb-f40e77aa15a2"
 
