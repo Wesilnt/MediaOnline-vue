bfdc1c0e6a6ad7c7e48cb75de0da828e9b04e817
liuchao <the_liuchao@sian.cn>
2018-11-27 16:58:20 +0800

    减少参数

--- src/components/share/DistributorShare.vue
+++ src/components/share/DistributorShare.vue
@@ -124,7 +124,7 @@
                 const href = -1 != location.href.indexOf('?')?location.href.split('?')[0]:location.href
                 const shareHref = `${href}${-1 != href.indexOf('?') ? '&' : '?'}`
                 const distributor = btoa(encodeURIComponent(JSON.stringify({id:user.id,avatarUrl:user.avatarUrl,nickName:user.nickName})))
-                this.shareUrl = `${shareHref}preUserId=${user.id}&distributor=${distributor}`
+                this.shareUrl = `${shareHref}preUserId=${user.id}`
                 console.log("shareUrl1:",this.shareUrl)
                 const shareData = {
                     title: '秦汉胡同在线',
