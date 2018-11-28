0d90d3641bb0a2b23586269b3eb91417a5c3ebb7
liuchao <the_liuchao@sian.cn>
2018-11-28 16:09:25 +0800

    修改环境

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,7 +4,7 @@
 const { NODE_ENV } = process.env
 
 // 调试选项，本地调试请改为 False
-export const IS_ONLINE = false;
+export const IS_ONLINE = true;
 // 测试 Token
 export const TEST_TOKEN = "60487b89-0503-4aa0-88c3-defe70607929"
 // export const TEST_TOKEN = 'd5baa57e-74ee-4dd4-8122-f9c0f360889f'
